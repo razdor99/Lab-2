946e2b2ab4c5f6cbbd0f8324e8a2d2124c4c2de7
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-09-28 22:03:26 +0000

    q

--- BlinkUsingThreadedCallbacks.py
+++ BlinkUsingThreadedCallbacks.py
@@ -14,25 +14,24 @@ pwm2 = GPIO.PWM(20, 1)
 pwm3 = GPIO.PWM(21, 1)
 
 def myCallback(pin):
-  try:
-    if pin == 20:
-      pwm1.start(0)
-      for d1 in range(101):
-        pwm1.ChangeDutyCycle(d1)
+  
+ if pin == 20:
+    pwm1.start(0)
+    for d1 in range(101):
+      pwm1.ChangeDutyCycle(d1)
+      sleep(0.005)
+    for d2 in range(101):
+      pwm1.ChangeDutyCycle(101 - d2)
+      sleep(0.005)
+  if pin == 21:
+    pwm2.start(0)
+    for d3 in range(101):
+      pwm2.ChangeDutyCycle(d3)
         sleep(0.005)
-      for d2 in range(101):
-        pwm1.ChangeDutyCycle(101 - d2)
-        sleep(0.005)
-    if pin == 21:
-      pwm2.start(0)
-      for d3 in range(101):
-        pwm2.ChangeDutyCycle(d3)
-        sleep(0.005)
-      for d4 in range(101):
-        pwm2.ChangeDutyCycle(101 - d4)
-        sleep(0.005)
-  except KeyboardInterrupt:
-    print('\nExiting')
+    for d4 in range(101):
+      pwm2.ChangeDutyCycle(101 - d4)
+      sleep(0.005)
+  
 
 
 GPIO.add_event_detect(5, GPIO.RISING, callback=myCallback,bouncetime=200)
