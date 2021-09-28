0762459a350621ba622accd6602fb51ed4d2f9fd
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-09-28 21:55:13 +0000

    everything

--- BlinkUsingThreadedCallbacks.py
+++ BlinkUsingThreadedCallbacks.py
@@ -2,18 +2,48 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-p = 4
-GPIO.setup(p, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-pwm = GPIO.PWM(p, 100)
 
-try:
-  pwm.start(0)
-  while 1:
-    for dc in range(101):
-      pwm.ChangeDutyCycle(dc)
-      sleep(0.01)
-except KeyboardInterrupt:
-  print('\nExiting')
+pwm1 = GPIO.PWM(4, 1)
+pwm2 = GPIO.PWM(20, 1)
+pwm3 = GPIO.PWM(21, 1)
 
-pwm.stop()
+def myCallback(pin):
+  try:
+    if pin == 20:
+      pwm1.start(0)
+      for d1 in range(101):
+        pwm1.ChangeDutyCycle(d1)
+        sleep(0.01)
+      for d2 in range(101):
+        pwm1.ChangeDutyCycle(101 - d2)
+        sleep(0.01)
+    if pin == 21:
+      pwm2.start(0)
+      for d3 in range(101):
+        pwm2.ChangeDutyCycle(d3)
+        sleep(0.01)
+      for d4 in range(101):
+        pwm2.ChangeDutyCycle(101 - d4)
+        sleep(0.01)
+    while 1:
+      pwm3.start(50)
+  except KeyboardInterrupt:
+    print('\nExiting')
+
+
+
+
+GPIO.add_event_detect(5, GPIO.RISING, callback=myCallback,bouncetime=500)
+GPIO.add_event_detect(6, GPIO.RISING, callback=myCallback,bouncetime=500)
+# Infinite loop:
+pwm1.stop()
+pwm2.stop()
+pwm3.stop()
+
+GPIO.cleanup()
